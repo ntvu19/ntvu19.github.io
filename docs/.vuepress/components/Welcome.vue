8186282df5359eb980d8999ebc1ccc4105250ac7
Vu T. Nguyen <vu.t.nguyen@opswat.com>
2024-07-20 20:32:15 +0700

    Add style

--- docs/.vuepress/components/Welcome.vue
+++ docs/.vuepress/components/Welcome.vue
@@ -1,6 +1,13 @@
 <template>
-    <p>Welcome! I'm Vu T. Nguyen</p>
-    <button>click me!</button>
+    <div>
+        <p>Hi!</p>
+        <p>My name is Thanh-Vu Nguyen, I have completed my bachelor's degree at University of Science (Ho Chi Minh City, Vietnam). During my academic journey, I earned the respect of my professors due to my diligent and delicated approach to learning.</p>
+        <p>I have a strong passion for acquiring new knowledge, particularly in the field of back-end software development. However, I am not limited to one area of expertise and find great fascination in exploring various domains such as DevOps, Software Architecture, and Artificial Intelligence, with a particular focus on Computer Vision.</p>
+        <p>I enjoy pushing myself by tackling challenging software development and system architecture problems. I have gained some practical experience in web application development through university projects.</p>
+        <p>My primary areas of interest include building highly practical software, honing problem solving skills, writing maintainable and scalable code, optimizing systems through source code and databases, ensuring user data security, and more.</p>
+        <p>During my free time, I like brainstorming ideas for software applications that cater to essential user needs and continually expanding my knowledge in these areas.</p>
+        <button>click me!</button>
+    </div>
 
 </template>
 
@@ -10,4 +17,8 @@ export default {
 }
 </script>
 
-<!-- <style></style> -->
+<style scoped>
+    * {
+        border: 1px solid black;
+    }
+</style>
--- docs/index.md
+++ docs/index.md
@@ -2,14 +2,16 @@
 lang: en-US
 title: Home
 description: route - /
-home: true
+sidebar: false
+# home: true
 # navbar: false
-# https://theme-hope.vuejs.press/guide/layout/home.html#demo
-bgImage: https://theme-hope-assets.vuejs.press/bg/6-light.svg
-bgImageDark: https://theme-hope-assets.vuejs.press/bg/6-dark.svg
-heroText: VuePress Theme Hope
-tagline: A VuePress theme with tons of features✨
 ---
 
 <Welcome/>
 <Contact/>
+
+<!-- <style>
+    .page .theme-default-content {
+        max-width: 1440px;
+    }
+</style> -->
