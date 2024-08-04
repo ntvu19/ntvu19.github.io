31270fd6dc091eed07e15facc69bad8053e00e4e
Vu T. Nguyen <vu.t.nguyen@opswat.com>
2024-08-04 13:05:35 +0700

    Add contact information

--- docs/.vuepress/components/Contact.vue
+++ docs/.vuepress/components/Contact.vue
@@ -4,7 +4,12 @@
             <span id="contact-header">CONTACT</span>
         </div>
         <div class="content">
-            <p>This is the contact info</p>
+            <!-- TODO: Move this component to the below position of the avatar in the `Welcome` component -->
+            <!-- https://icon-sets.iconify.design/logos/ -->
+            <a href="https://facebook.com/msc.thanhvu" target="_blank"><img id="icon-facebook" src="/icon/contact/logos--facebook.svg" alt="Facebook" draggable="false"></a>
+            <a href="https://github.com/ntvu19" target="_blank"><img id="icon-github" src="/icon/contact/logos--github-icon.svg" alt="GitHub" draggable="false"></a>
+            <a href="mailto:msc.thanhvu@gmail.com" target="_blank"><img id="icon-gmail" src="/icon/contact/logos--google-gmail.svg" alt="Gmail" draggable="false"></a>
+            <a href="https://linkedin.com/in/nguyenthanhvu" target="_blank"><img id="icon-linked" src="/icon/contact/logos--linkedin-icon.svg" alt="LinkedIn" draggable="false"></a>
         </div>
     </div>
     
@@ -12,7 +17,18 @@
 
 <script>
 export default {
-    name: 'Contact'
+    name: 'Contact',
+    data() {
+        return {
+            //
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+        //
+    }
 }
 </script>
 
