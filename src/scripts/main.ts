7563261f881e298d71dcfeae298df2fa2d9afec9
Vu T. Nguyen <msc.thanhvu@gmail.com>
2026-04-25 23:25:08 +0700

    Bootstrap keyboard, section tracker, theme handlers

---
+++ src/scripts/main.ts
@@ -0,0 +1,6 @@
+import { activateKeyboard, activateSectionTracker } from './keyboard';
+import { bindToggleHandlers } from './theme';
+
+activateKeyboard();
+activateSectionTracker();
+bindToggleHandlers();
